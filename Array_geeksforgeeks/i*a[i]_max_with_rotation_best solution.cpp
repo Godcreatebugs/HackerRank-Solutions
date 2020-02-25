e76a182f0191a72707dd84634bbf6d1cd4baad41
Godcreatebugs <bhuvapiyush63@gmail.com>
2020-02-25 17:10:20 -0500

    time complexity linear

--- Array_geeksforgeeks/i*a[i]_max_with_rotation_best solution.cpp
+++ Array_geeksforgeeks/i*a[i]_max_with_rotation_best solution.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<array>
 using namespace std;
 // So this problem is more about the mathematical progression and
 // reducing the time complecity of the problem to O(n)
@@ -11,17 +12,27 @@ int MakeSum(int arr[],int n){
   }
 return sum;
 }
-
+int accumulate(int arr[],int n){
+  int sum=0;
+  for(int i=0;i<n;i++){
+    sum+=arr[i];
+  }
+return sum;
+}
  int Max_Product(int arr[],int n){
-   int arr_0=MakeSum(arr,n);
-   int arr_j_1 = arr_0;
+   int arr_j_1=MakeSum(arr,n);
+   //int arr_j_1 = arr_0;
+   int sum_array=accumulate(arr,n);
    int max_ele_product;
    for(int j=1;j<n;j++){
       int arr_j;
       //int max_ele_product;
-      arr_j = arr_j_1 + arr_0 -(n)*arr[n-j];
+      arr_j = arr_j_1 + sum_array -(n)*arr[n-j];
+      cout<<arr_j<<" ";
       if (arr_j>arr_j_1){
       max_ele_product=arr_j;
+      cout<<"\n";
+      cout<<max_ele_product;
       }
       arr_j_1=arr_j;
 
@@ -31,8 +42,10 @@ return sum;
  }
 
  int main(){
-   int arr[]={5,1,2,3,4};
+   int arr[]={1,2,3,4,5};
     int max_product=Max_Product(arr,5);
+    //int sum=MakeSum(arr,5);
     cout<<max_product;
+
    return 0;
  }
