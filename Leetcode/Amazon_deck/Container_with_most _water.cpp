9596024bd51e8f2dcbe9c990f21f99d7bb05c60d
Godcreatebugs <bhuvapiyush63@gmail.com>
2020-07-04 17:19:11 -0400

    Perfect Sol.

--- Leetcode/Amazon_deck/Container_with_most _water.cpp
+++ Leetcode/Amazon_deck/Container_with_most _water.cpp
@@ -2,23 +2,41 @@
 using namespace std;
 
 int MaxArea(vector<int> &height){
- int n  =height.size();
-int max_area = INT8_MIN; //update it with max
-//Setting up limits at the beginning
-int i = 0;
-int j = n-1;
-while (i<j){
-    int min_height = min(height[i],height[j]);//constraint
-    int max_area = max(max_area, min_height * (j-i));// only update if max_area has bumped
-    // concentrating one one bar which is higher and move other bar accordingly
-    // We are updating it just when we need it. Max_area is updated when we need it.
-    if (height[i]>height[j])
-        j--;
-    else
-        i++;
-}
-return max_area;
+
+  /* METHOD 1 : Brute Force Solution iterate using 2 for loops untill you find best area. and time limit exceed.O(n^2)
+  int max_area= 0;
+  int n = height.size();
+  for (int i=0;i<n-1;i++){
+      for(int j=i+1;j<n;j++){
+          int min_height  = min(height[i],height[j]);
+          if (min_height*(j-i)>max_area) max_area = min_height * (j-i);
+      }
+  }
+          return max_area;
+      }
+      };
+  */
+
+  //METHOD 2 : This one is a little tricky, and you focus on the bigger bar and limiting factor which is min of height of 2 bars.
+  // And we update only we execeed previous max, IDK why my precious code didnt work it was the same.
+  int max = INT8_MIN;
+  int min=0;
+  int i =0;
+  int j = height.size()-1;
+  while(i<j){
+    min = std::min(height[i],height[j]);
+    max = std::max(max,(j-i)*min);
+    if (height[i]>height[j]){
+      j--;
     }
+    else{
+      i++;
+    }
+  }
+  return max;
+
+      }
+    
 
 
 int main(){
