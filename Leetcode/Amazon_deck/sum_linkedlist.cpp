fc0d0a343512268492b5f3e398fcccea703aeef9
Godcreatebugs <bhuvapiyush63@gmail.com>
2020-07-07 11:58:37 -0400

    Solution in max(m,n).

--- Leetcode/Amazon_deck/sum_linkedlist.cpp
+++ Leetcode/Amazon_deck/sum_linkedlist.cpp
@@ -12,16 +12,49 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *res_node = new ListNode(); //allocation of memory on heap
-        res_node->data =0;
-        res_node->next =null;
+        /* Some important assumptions:
+        List of both the linkedlist may be different and we have to max(m,n) complexity option.
+        Carry from sum has to carry forward.(Do it using modulo operation)
+        Make a new node every single time you add elements, dynamically allocated memory using constructor.
+        use while condition to check both of list hasn;t hit null which is end of op in list.
+        */
+        int sum =0;
+        // res is only a read only memory and commented part below  changing res directly is an error machine
+        //It is best to access res using a pointer to res address as it is not read only
+        ListNode* res = NULL; // begining with null but store res address where we start our op.
+        ListNode** node = &res; //a pointer where we have stored l3  to make op possible.
 
-        while(l1->next != null && l2->next  != null ){
+        /*
+        [5]
+         [5] should give [0,1] hence we need to make sure sum reduces to 0 and no carry is forwarded
+        If sum is zero no need to go ahead and make a new node
+        */
 
-            if(l1->val + l2->val <9){
-                res_node->val = l1->val + l2->val;
-                res_node->p
+        while(l1 != NULL || l2 !=NULL || sum>0){
+            if (l1 != NULL){
+                sum += l1->val;
+                l1 = l1->next;
             }
+            if (l2 != NULL){
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            // This is dangours way to work as we just have res's address for ref and res=res->next is dumb
+            //thing to do as it will result in undefined behaviour and maily since res is Read Only Memory
+            // We can access it via its address via a pointer to res's address in other words node
+            // res = new ListNode(sum%10);
+            // sum /=10;
+            // res = res->next;
+            *node = new ListNode(sum%10); // (*node == res)
+            //carry has to get into sum like
+            sum /= 10;
+            //since we have taken node as a reference
+            //remember address is not same as pointer hence *node = &res , dereferncing node won't give you res's adress
+            //Only way to change pointer which lies in node and point to next and node's adress changes.
+            //Its difficult to process but try to understand it..
+            //*node = *node->next
+            node =&((*node)->next);
         }
+        return res;
     }
 };
