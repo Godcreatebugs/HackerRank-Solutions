5df009922aab882019f9e537dfa598b742b9af70
Godcreatebugs <bhuvapiyush63@gmail.com>
2020-07-10 14:50:34 -0400

    hashmap is casuing heap corruption.

--- Leetcode/Amazon_deck/missing_number.cpp
+++ Leetcode/Amazon_deck/missing_number.cpp
@@ -11,7 +11,7 @@ public:
         sort(nums.begin(),nums.end());
         //edge cases
         if(nums[0]!=0) return 0;
-        if(nums[n-1] != n-1) return n-1;
+        if(nums[n-1] != n) return n;
 
         //traversing to compare 2 consecutive elements
         for(int i=1;i<n;i++){
@@ -20,34 +20,38 @@ public:
         }
         //if none of the cases above and btw -1 is never part of array
         return -1;
-        //THE ANSWER IS WRONG , [0] SHOULD GIVE -1 WHICH MY CODE IS GIVING as we are missing nothing.
+        //THE ANSWER IS RIGHT NOW BUT THE DESCRIPTION IS WRONG OF QUESTION.
         */
 
 
         /*
-        Method 2: We can harness power of hasmap in c++ which offer linear time search for each element using key-             value pair. And we can compare current traversing element to the element+1  and if we havent find that element         we have got our match.
-
-        int n =nums.size();
+        Method 2: We can harness power of hasmap in c++ which offer linear time search for each element using key-             value           pair. And we can compare current traversing element to the element+1  and if we havent find that element         we have got           our match.
+        */
+        int n = nums.size();
         unordered_map <int,int> lookup_hash;
         for (int i=0;i<n;i++){
-            lookup_hash[i] = nums[i]; //fulfilled elements in hashmap
+            lookup_hash[nums[i]] = i; //fulfilled elements in hashmap
         }
-        for (int i=0;i<n;i++){
-            if (lookup_hash.find(i) == lookup.end()){
-                return
+        for (int i=0;i<n+1;i++){
+            if (lookup_hash.find(nums[i]) == lookup_hash.end()){
+                return nums[i];
             }
         }
-        */
+        return -1;
+    }
+
         /*
         METHOD 4: This is badass way to do this question you use the sum of numbers formula and do the sum using for           loop of all the number then the differnce is the missing number and remember what is the sum_sq-sum is 0 then         your array is complete.
-        */
+       \
         int n = nums.size();
         int gauss_sum = n* (n+1) / 2 ;
         int sum{0};
         for (int i=0;i<n;i++){
             sum += nums[i];
         }
-        //what is difference is 0 you can not say that 0 is missing
+
         return gauss_sum - sum;
+
         }
+        */
 };
