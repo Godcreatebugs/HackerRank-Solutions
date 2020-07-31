aa5cb9879489ee93a2d51a48dad2827c54db37d1
Godcreatebugs <bhuvapiyush63@gmail.com>
2020-07-31 09:39:10 -0400

    revision of  sum

---
+++ Leetcode/Amazon_deck/two_sum_revision.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector <int> TwoSum(vector <int> arr,int target){
+  vector <int> res;
+  int n = arr.size();
+  unordered_map <int,int> hash;
+
+/* BRUTE FORCE SOLUTION
+  for(int i=0;i<n-1;i++){
+    for(int j=i+1;j<n;j++){
+      if (arr[i]+arr[j]==target){
+        res = {arr[i],arr[j]};
+      }
+    }
+  }
+  return res;
+
+*/
+
+//HASHMAP SOLUTION O(N) TIME COMPLEXITY
+//decelaration of hah_table
+for(int i=0;i<n;i++){
+  int num_find = target - arr[i];
+  if (hash.find(num_find) != hash.end()){
+       res ={arr[i],num_find};
+  }
+  hash[arr[i]]=i;
+}
+return res;
+}
+
+int main(){
+  vector <int> arr {2,11,5,7};
+  vector <int> res = TwoSum(arr,9);
+  for(int i=0;i<2;i++){
+    cout<<res[i]<<" ";
+  }
+  return 0;
+}
