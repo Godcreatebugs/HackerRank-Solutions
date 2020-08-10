502914830ae66c7fd76a63a5699ff36cdd65ed42
Godcreatebugs <bhuvapiyush63@gmail.com>
2020-08-10 08:53:57 -0400

    perfect solution with testcase disturbance.

--- Leetcode/Bloomberg_Deck/tree/validate_BST_inorder.cpp
+++ Leetcode/Bloomberg_Deck/tree/validate_BST_inorder.cpp
@@ -2,6 +2,15 @@
 #include <stack>
 using namespace std;
 //treenode struct
+/*
+Why this solution is better?
+-> It is in a way faster than recursive solution even though time complexity is asymptotically same.
+-> The reason is here the detection of failure is easy.The worst case only happens when rogue element is right most element
+-> And space complxity is same but instead of recursive calls taking up space to store true and false we need a stack and stack op are faster.
+*/
+
+
+
 struct TreeNode{
   int val;
   TreeNode* right;
@@ -19,29 +28,35 @@ struct TreeNode{
 
 bool ValidBST(TreeNode* root){
   stack<TreeNode*> s;
-  int new_ele =INT32_MIN;
+  int prev =INT32_MIN;
   while(!s.empty() || root !=nullptr){
     //poplating only the leftmost part of tree
+    // we have to consider a case where root = root->root->right is going to give nullptr
     while(root!=nullptr){
       s.push(root);
       root =root->left;
     }
     //starting root from below to make a list of numbers in ascendancy
-    root = s.top();
+    //pop element so wont be in stack anymore
+    root = s.top(),s.pop();
     //fasle condition is used in recursion to move ahead and not stop function abruptly
-    if(root->val<=new_ele) return false;
-    new_ele = root->val;
-    root =root->right;
+    if(root->val<=prev) return false;
+    prev = root->val;
+    root = root->right;
   }
   return true;
 }
 
 int main(){
-  TreeNode* root = new TreeNode(2);
-  TreeNode* right = new TreeNode(3);
+  TreeNode* root = new TreeNode(5);
+  TreeNode* right = new TreeNode(8);
   TreeNode* left = new TreeNode(1);
+  TreeNode* right2 =new TreeNode(9);
+  TreeNode* left2 =new TreeNode(6);
 
   root->right = right;
   root->left = left;
+  right->right = right2;
+  right->left =left2;
   cout<<ValidBST(root);
 }
